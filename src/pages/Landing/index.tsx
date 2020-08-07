b8e6243fd4e787cb523d4da7c0da676d33642fd3
Almerindo Paixão <almerindopaixao@gmail.com>
2020-08-07 11:46:48 -0300

    Update Total Connections

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
+
+import api from '../../services/api';
 
 import styles from './styles';
 
@@ -11,6 +13,15 @@ import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
 export default function Landing() {
+    const [totalConnections, setTotalConnections] = useState(0);
+
+    useEffect(() => {
+      api.get('connections').then((response) => {
+        const { total } = response.data;
+
+        setTotalConnections(total);
+      })
+    }, []);
 
     const { navigate } = useNavigation();
 
@@ -53,7 +64,7 @@ export default function Landing() {
         </View>
 
         <Text style={styles.totalConnections}>
-            Total de 285 conexões já realizadas {' '}
+            Total de {totalConnections} conexões já realizadas {' '}
             <Image source={heartIcon} />
         </Text>
       </View>
