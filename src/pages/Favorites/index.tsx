833ddea39aca75caddcb90e5d96883af604a2b10
Almerindo Paixão <almerindopaixao@gmail.com>
2020-08-07 10:12:29 -0300

    Update Teacher List

--- src/pages/Favorites/index.tsx
+++ src/pages/Favorites/index.tsx
@@ -1,14 +1,30 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ScrollView } from 'react-native';
 import PageHeader from '../../components/PageHeader';
 
-import styles from './styles';
+import TeacherItem from '../../components/TeacherItem';
 
+import styles from './styles';
 
 export default function Favorites() {
   return (
     <View style={styles.container}>
       <PageHeader title="Meus proffys favoritos" />
+
+      <ScrollView
+        style={styles.teacherList}
+        contentContainerStyle={{
+          paddingHorizontal: 16,
+          paddingBottom: 16,
+        }}
+      >
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+        <TeacherItem />
+      </ScrollView>
     </View>
   );
 }
